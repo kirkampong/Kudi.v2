ebf0fe683074aec5df3a5bb52147a8584b6ed899
Kirk Ampong <kirkampong2@gmail.com>
2021-02-16 21:20:17 +0000

    team member cards

---
+++ build/precache-manifest.7a5ec83ee2e83d0989a9a11e0a61fda7.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61c079acdf5d223a0c35797c53391df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03b5a629fedb30becffa",
+    "url": "/static/css/2.bda0e5a4.chunk.css"
+  },
+  {
+    "revision": "cee551faeb9863dd85a7",
+    "url": "/static/css/main.0e07a94f.chunk.css"
+  },
+  {
+    "revision": "03b5a629fedb30becffa",
+    "url": "/static/js/2.86f4d2b8.chunk.js"
+  },
+  {
+    "revision": "821079347f407a814640c34869cd8e2f",
+    "url": "/static/js/2.86f4d2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee551faeb9863dd85a7",
+    "url": "/static/js/main.29b6fd99.chunk.js"
+  },
+  {
+    "revision": "a46bfcdcf5140429733b6beade93c124",
+    "url": "/static/js/main.29b6fd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55b84c65c092ec71c90",
+    "url": "/static/js/runtime-main.bcab16a1.js"
+  },
+  {
+    "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
+    "url": "/static/media/fa-brands-400.0c9f225e.woff2"
+  },
+  {
+    "revision": "0ce1e868452204695c8ac1c70f693c2d",
+    "url": "/static/media/fa-brands-400.0ce1e868.woff"
+  },
+  {
+    "revision": "5d10d00980077f67990f73b1961bc46e",
+    "url": "/static/media/fa-brands-400.5d10d009.svg"
+  },
+  {
+    "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
+    "url": "/static/media/fa-brands-400.d28c96ca.eot"
+  },
+  {
+    "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
+    "url": "/static/media/fa-brands-400.dfe5aa43.ttf"
+  },
+  {
+    "revision": "0232fe8b09de812c88e6cc27f57a3a88",
+    "url": "/static/media/fa-regular-400.0232fe8b.eot"
+  },
+  {
+    "revision": "398be22b2308585b84da8e858e2d7b60",
+    "url": "/static/media/fa-regular-400.398be22b.ttf"
+  },
+  {
+    "revision": "3cace4a04d941b5981ba32c6ce9afae1",
+    "url": "/static/media/fa-regular-400.3cace4a0.woff"
+  },
+  {
+    "revision": "847712aaabbeba674afdda86d31cab17",
+    "url": "/static/media/fa-regular-400.847712aa.woff2"
+  },
+  {
+    "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
+    "url": "/static/media/fa-regular-400.d0fdc7c2.svg"
+  },
+  {
+    "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
+    "url": "/static/media/fa-solid-900.00bb62bc.eot"
+  },
+  {
+    "revision": "2306f642b31b774ba1d8b6b784539e19",
+    "url": "/static/media/fa-solid-900.2306f642.svg"
+  },
+  {
+    "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
+    "url": "/static/media/fa-solid-900.260d033f.ttf"
+  },
+  {
+    "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
+    "url": "/static/media/fa-solid-900.4bfbf7eb.woff"
+  },
+  {
+    "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
+    "url": "/static/media/fa-solid-900.9ae050d1.woff2"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/static/media/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "861720fa94b80958075f620b2c6e8fb9",
+    "url": "/static/media/nucleo-icons.861720fa.woff"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/static/media/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/static/media/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/static/media/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "6dcd18dcc045aaf0d5db63a112747311",
+    "url": "/static/media/photo-1431578500526-4d9613015464.6dcd18dc.jpeg"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/static/media/sidebar-2.7a4ce7cc.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/static/media/sidebar-3.34535b95.jpg"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/static/media/sidebar-4.7bc259fd.jpg"
+  }
+]);
---
+++ src/views/About/About.js
@@ -0,0 +1,237 @@
+import React from "react";
+import './About.css';
+
+// react-bootstrap components
+import {
+  Badge,
+  Button,
+  Card,
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+
+function Typography() {
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col md="12">
+            <Card>
+              <Card.Header>
+                <Card.Title as="h4">Our Team</Card.Title>
+              </Card.Header>
+              <Card.Body>
+
+                <Row>
+                  <Col md="4">
+                    <Card>
+                      <Card.Header>
+                        <p className="card-category description text-center">CEO/CTO: Kirk Ampong </p>
+                      </Card.Header>
+                      <Card.Body>
+                        <div className="team-member">
+                          <img class="img-responsive" src={require('./krik.jpg')} />
+                        </div>
+                      </Card.Body>
+                    
+                    <Card.Footer>
+                      <div class="d-flex justify-content-between">
+                        <Button
+                          className="btn-social btn-outline btn-round sharrre"
+                          id="twitter"
+                          variant="twitter"
+                          href="https://twitter.com/opanyin_k"
+                        >
+                          <i className="fab fa-twitter"></i> 
+                        </Button>
+                        <Button
+                          className="btn-social btn-outline btn-round sharrre"
+                          id="instagram"
+                          variant="instagram"
+                          href="https://www.instagram.com/kirko__/"
+                        >
+                          <i class="fab fa-instagram"></i> 
+                        </Button>
+                        <Button
+                          className="btn-social btn-outline btn-round sharrre"
+                          id="linkedin"
+                          variant="linkedin"
+                          href="https://www.linkedin.com/in/kirkampong"
+                        >
+                          <i class="fab fa-linkedin-in"></i> 
+                        </Button>
+                      </div>
+                    </Card.Footer>
+                   </Card>
+                  </Col>
+
+
+                  <Col md="4">
+                    <Card>
+                      <Card.Header>
+                        <p className="card-category description text-center">COO/Product Manager: Joel Ojo</p>
+                      </Card.Header>
+                      <Card.Body>
+                        <div className="team-member">
+                          <img class="img-responsive" src={require('./joski.jpg')} />
+                        </div>
+                      </Card.Body>
+                    
+                    <Card.Footer>
+                      <div class="d-flex justify-content-between">
+                        <Button
+                          className="btn-social btn-outline btn-round sharrre"
+                          id="twitter"
+                          variant="twitter"
+                          href="https://twitter.com/opanyin_k"
+                        >
+                          <i className="fab fa-twitter"></i> 
+                        </Button>
+                        <Button
+                          className="btn-social btn-outline btn-round sharrre"
+                          id="instagram"
+                          variant="instagram"
+                          href="https://www.instagram.com/kirko__/"
+                        >
+                          <i class="fab fa-instagram"></i> 
+                        </Button>
+                        <Button
+                          className="btn-social btn-outline btn-round sharrre"
+                          id="linkedin"
+                          variant="linkedin"
+                          href="https://www.linkedin.com/in/kirkampong"
+                        >
+                          <i class="fab fa-linkedin-in"></i> 
+                        </Button>
+                      </div>
+                    </Card.Footer>
+                   </Card>
+                  </Col>
+
+
+
+                </Row>
+
+
+                <div className="typography-line">
+                  <h1>
+                    <span>Header 1</span>
+                    The Life of Light Bootstrap Dashboard React
+                  </h1>
+                </div>
+                <div className="typography-line">
+                  <h2>
+                    <span>Header 2</span>
+                    The Life of Light Bootstrap Dashboard React
+                  </h2>
+                </div>
+                <div className="typography-line">
+                  <h3>
+                    <span>Header 3</span>
+                    The Life of Light Bootstrap Dashboard React
+                  </h3>
+                </div>
+                <div className="typography-line">
+                  <h4>
+                    <span>Header 4</span>
+                    The Life of Light Bootstrap Dashboard React
+                  </h4>
+                </div>
+                <div className="typography-line">
+                  <h5>
+                    <span>Header 5</span>
+                    The Life of Light Bootstrap Dashboard React
+                  </h5>
+                </div>
+                <div className="typography-line">
+                  <h6>
+                    <span>Header 6</span>
+                    The Life of Light Bootstrap Dashboard React
+                  </h6>
+                </div>
+                <div className="typography-line">
+                  <p>
+                    <span>Paragraph</span>I will be the leader of a company that
+                    ends up being worth billions of dollars, because I got the
+                    answers. I understand culture. I am the nucleus. I think
+                    that’s a responsibility that I have, to push possibilities,
+                    to show people, this is the level that things could be at.
+                  </p>
+                </div>
+                <div className="typography-line">
+                  <span>Quote</span>
+                  <blockquote>
+                    <p className="blockquote blockquote-primary">
+                      "I will be the leader of a company that ends up being
+                      worth billions of dollars, because I got the answers. I
+                      understand culture. I am the nucleus. I think that’s a
+                      responsibility that I have, to push possibilities, to show
+                      people, this is the level that things could be at."{" "}
+                      <br></br>
+                      <br></br>
+                      <small>- Noaa</small>
+                    </p>
+                  </blockquote>
+                </div>
+                <div className="typography-line">
+                  <span>Muted Text</span>
+                  <p className="text-muted">
+                    I will be the leader of a company that ends up being worth
+                    billions of dollars, because I got the answers...
+                  </p>
+                </div>
+                <div className="typography-line">
+                  <span>Primary Text</span>
+                  <p className="text-primary">
+                    I will be the leader of a company that ends up being worth
+                    billions of dollars, because I got the answers...
+                  </p>
+                </div>
+                <div className="typography-line">
+                  <span>Info Text</span>
+                  <p className="text-info">
+                    I will be the leader of a company that ends up being worth
+                    billions of dollars, because I got the answers...
+                  </p>
+                </div>
+                <div className="typography-line">
+                  <span>Success Text</span>
+                  <p className="text-success">
+                    I will be the leader of a company that ends up being worth
+                    billions of dollars, because I got the answers...
+                  </p>
+                </div>
+                <div className="typography-line">
+                  <span>Warning Text</span>
+                  <p className="text-warning">
+                    I will be the leader of a company that ends up being worth
+                    billions of dollars, because I got the answers...
+                  </p>
+                </div>
+                <div className="typography-line">
+                  <span>Danger Text</span>
+                  <p className="text-danger">
+                    I will be the leader of a company that ends up being worth
+                    billions of dollars, because I got the answers...
+                  </p>
+                </div>
+                <div className="typography-line">
+                  <h2>
+                    <span>Small Tag</span>
+                    Header with small subtitle <br></br>
+                    <small>Use "small" tag for the headers</small>
+                  </h2>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Typography;
